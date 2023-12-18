708168c30c5e1cff5305cd9f73fc32d5a9421a26
Javier <jmartin@grupoloyal.com>
2023-12-18 15:10:45 +0100

    ejemplos

--- curso/src/app/main/index.ts
+++ curso/src/app/main/index.ts
@@ -1,2 +1,6 @@
-export * from './header/header.component'
+export * from './notification/notification.component'
+export * from './notification-modal/notification-modal.component'
+export * from './home/home.component'
+export * from './ajax-wait'
 export * from './page-not-found/page-not-found.component'
+export * from './header/header.component'
