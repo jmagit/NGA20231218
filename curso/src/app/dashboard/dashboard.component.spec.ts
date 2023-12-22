cdd92531731b33fec01acf1d45fd2d71f1161965
Javier <jmartin@grupoloyal.com>
2023-12-22 13:27:11 +0100

    e2e

--- curso/src/app/dashboard/dashboard.component.spec.ts
+++ curso/src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-xdescribe('DashboardComponent', () => {
+describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [DashboardComponent, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
 
